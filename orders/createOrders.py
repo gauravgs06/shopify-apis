e7041c4989dce65c1005e3e6242c0eac32040c9c
Gaurav Sharma <gauravsharmags06@gmail.com>
2023-10-26 12:19:46 +0530

    Order Creation Added

--- orders/createOrders.py
+++ orders/createOrders.py
@@ -1,43 +1,91 @@
 import requests
 import json
-import pandas
+import pandas, numpy, time
 
 brand = "tac"
 with open("./config.json", "r") as file:
     config = json.load(file)[brand]
 
-base_url = config['shop_url']
-api_version = config['api_version']
+base_url = config["shop_url"]
+api_version = config["api_version"]
 resource_url = "orders"
 
-params = {
-    "created_at_min": "2022-11-01T00:00:00+05:30",
-    "limit": 250,
-    "status":"any"
-}
+input = pandas.read_excel("Dispatch - IM (4).xlsx",sheet_name="25th Oct.")
+productList = pandas.read_csv("products-tac-2023-Oct-25.csv").set_index("handle")
+params = {"api_version": "2023-10"}
+
 headers = {
-    "X-Shopify-Access-Token": config['api_token']
+    "X-Shopify-Access-Token": config["api_token"],
+    "Content-Type": "application/json",
 }
-
-try:
-    iter = 0
-    response = requests.get(base_url + "/admin/api/" + api_version + "/" + resource_url + ".json", params=params, headers=headers)
-    orders  = response.json()['orders']
-    # total = response.headers['']
-    next_url = None
-    if response.links['next']:
-        next_url = response.links['next']['url']
-    while next_url:
-        response = requests.get(next_url, headers=headers)
-        orders += response.json()['orders']
-        iter += 1
-        print("completed",iter,"iterations")
-        if response.links['next']:
-            next_url = response.links['next']['url']
+order_numbers = []
+for i in range(input.shape[0]):
+    try:
+        row = input.iloc[[i]].reset_index()
+        line_items = []
+        if brand == "dev":
+            line_items.append({"variant_id": 44456083915068, "quantity": 1})
         else:
-            next_url = None
-except Exception as e:
-    print(e)
+            try:    
+                for handle in row["Product Handle"][0].split("\n"):
+                    try:
+                        line_items.append(
+                            {
+                                "variant_id": str(productList.loc[handle.strip()]["Variant ID"]),
+                                "quantity": 1,
+                            }
+                        )
+                    except Exception as e:
+                        print("SKU Error:", e)
+            except Exception as e:
+                print("Error:",e)
+        address = {
+            "first_name": (row["FIRST NAME"][0]).strip(),
+            "last_name": str(row["Last Name"][0]).strip()
+            if str(row["Last Name"][0]) != "nan"
+            else "IM",
+            "address1": str(row["Address - 1"][0]).replace("\n", " ").strip(),
+            "address2": (row["Address - 2"][0]).strip()
+            if str(row["Address - 2"][0]) != "nan"
+            else " ",
+            "phone": str(row["Phone"][0]).strip()[:10],
+            "zip": str(row["Pincode"][0]).strip(),
+            "city": (row["City"][0]).strip(),
+            "province": (row["State Code"][0]).strip(),
+            "country": "India",
+        }
+        payload = {
+            "order": {
+                "first_name": (row["FIRST NAME"][0]).strip(),
+                "last_name": str(row["Last Name"][0]).strip()
+                if str(row["Last Name"][0]) != "nan"
+                else "IM",
+                "line_items": line_items,
+                "shipping_address": address,
+                "email": str(row["Email ID"][0]).strip(),
+                "phone": str(row["Phone"][0]).strip()[:10],
+                "financial_status": "paid",
+                "payment_gateway_names": ["manual"],
+                "discount_codes": [
+                    {"code": "IM100", "amount": "100", "type": "percentage"}
+                ],
+                "tags":"IM"
+            }
+        }
 
-pandas.DataFrame(orders).to_csv(f'orders-{brand}.csv',index=False)
+        response = requests.post(
+            base_url + "/admin/api/" + api_version + "/" + resource_url + ".json",
+            headers=headers,
+            data=json.dumps(payload),
+        )
+        print(response.status_code)
+        order_numbers.append(response.json()["order"]["name"])
+        time.sleep(.52)
+    except Exception as e:
+        print(e)
+        order_numbers.append("Error")
+
+# pandas.DataFrame(orders).to_csv(f"orders-{brand}.csv", index=False)
 print("Completed All")
+input["Order No"] = order_numbers
+input.to_excel("Orders-Created.xlsx", index=False)
